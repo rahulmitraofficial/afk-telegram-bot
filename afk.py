dc9e9072b913ac6097732a09cfb8da98f47801c9
Pranaov S <pranaov@hotmail.com>
2020-09-02 09:12:10 +0530

    Changed (user_id = user_id) to (user_id = user_id).first() in line 57
    Chsnged to get the mention by first name while a user's username is called.

--- afk.py
+++ afk.py
@@ -54,7 +54,7 @@ def rm(user_id):
 	if not get(user_id):
 		return False
 	try:
-		queryr = session.query(AFK).filter_by(user_id = user_id)
+		queryr = session.query(AFK).filter_by(user_id = user_id).first()
 		queryr.delete()
 		session.commit()
 		return True
