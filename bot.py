dc83382c58bcce7d2c6f16c0c371b0800f1d0d2d
Roj Serbest <49933115+rojserbest@users.noreply.github.com>
2020-09-02 15:52:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -87,7 +87,8 @@ Reason: <b>{}</b>
 		""".format(mention, reason), parse_mode = "HTML")
 
 def private(update, context):
-	update.message.reply_text("Hello. This AFK Bot has no functions in PM. To use this bot in your groups, you need to host your own bot")
+	reply_markup = InlineKeyboardMarkup[InlineKeyboardButton(text = "Bot Repository", url = "https://github.com/rojserbest/caps-tv-afk-telegram-bot")]
+	update.message.reply_text("Hello. This AFK Bot has no functions in PM. To use this bot in your groups, you need to host your own bot", reply_markup = reply_markup)
 
 def main(update, context):
 	if update.message.from_user.username:
