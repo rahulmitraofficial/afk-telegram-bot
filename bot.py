04447a0211c2039d02096dea985c518504de7bf6
Pranaov S <pranaov@hotmail.com>
2020-09-03 08:56:47 +0530

    try random gif

--- bot.py
+++ bot.py
@@ -108,7 +108,8 @@ To use AFK features, you will need to host your own bot. Detailed instructions g
 
 If you don't wish to clone and self host, you can try @MissStella_bot instead, which has integrated AFK Features."""
 			reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text = "🛠 Bot Repository", url = "https://github.com/pranaovs/afk-telegram-bot")]])
-			update.message.reply_document("https://raw.githubusercontent.com/pranaovs/afk-telegram-bot/master/files/gif/leave/1.gif", caption = caption, reply_markup = reply_markup)
+			random_leavegif = random.randrange(1,3)
+			update.message.reply_document("https://raw.githubusercontent.com/pranaovs/afk-telegram-bot/master/files/gif/leave/" + random_leavegif + ".gif", caption = caption, reply_markup = reply_markup)
 			context.bot.leave_chat(update.message.chat.id)
 
 updater = Updater(os.environ.get("TOKEN"), use_context = True)
