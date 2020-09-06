e2dbf93d8493a83e18ee5dd739e23cec29a7219a
Roj Serbest <49933115+rojserbest@users.noreply.github.com>
2020-09-06 21:13:13 +0300

    Useless

--- users.py
+++ users.py
@@ -1,8 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
